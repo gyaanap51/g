326fa8028ce16a32151061862f87724b37f5ce44
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-11-11 11:58:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function room1(){
+    window.location ="room1.html";
+}
+function room2(){
+    window.location ="room2.html";
+}
---
+++ room1.js
@@ -0,0 +1,49 @@
+image1 = "";
+status = "";
+object = [];
+
+function preload(){
+ image1=loadImage('IMG_2059.jpg');
+}
+function back(){
+    window.location = "index.html";
+}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(image1, gotResult);
+}
+function gotResult(error, results) {
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object = results;
+}
+function draw(){
+image(image1, 0, 0, 640, 480);
+if(status !="")
+{
+    for (i =0; i<object.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Status :P Object Dectected";
+
+        fill("#FF0000")
+        percent = floor(object[i].confidence *100);
+        text(object[i].label + " " + percent + "%", object[i].x+15, object[i].y+15);
+        noFill();   
+        stroke("#FF0000");
+        rect(object[i].x, object[i].y, object[i].width, object[i].height);
+    }
+}
+   
+    
+}
